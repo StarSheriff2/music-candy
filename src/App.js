667d39aae2484923008c2f284a1007b31b9a5e2e
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-02-24 03:38:19 -0600

    Fix navigation to bottom of app

--- src/App.js
+++ src/App.js
@@ -1,49 +1,59 @@
 import React from 'react';
-import { BrowserRouter, Link, Routes, Route, Outlet } from 'react-router-dom';
+import {
+  BrowserRouter, Link, Routes, Route, Outlet,
+} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faRecordVinyl } from '@fortawesome/free-solid-svg-icons';
 import SearchPage from './pages/SearchPage/SearchPage';
 import Collection from './pages/Collection/Collection';
-import { height, style } from 'dom-helpers';
 
 function App() {
   const styles = {
     nav: {
-      border: "solid blue 2px",
-      height: "7rem",
+      border: 'solid blue 2px',
+      height: '7rem',
+      position: 'fixed',
+      bottom: '0',
+      width: '100%',
+      color: 'orange',
     },
     link: {
-      width: "100%",
+      width: '100%',
     },
     linkIcon: {
-      fontSize: "4rem",
-      color: "orange",
+      color: 'orange',
+      fontSize: '4rem',
     },
   };
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-          <div className="border border-danger">
-            <SearchPage />
-          </div>
-          }
+        <Route
+          path="/"
+          element={(
+            <div className="border border-danger">
+              <SearchPage />
+            </div>
+        )}
         />
-        <Route path="collection" element={<Collection />}/>
+        <Route path="collection" element={<Collection />} />
       </Routes>
-        <nav
-          style={styles.nav}
-          className="d-flex"
-        >
-          <Link to="/" className="d-flex" style={styles.link}>
-            <FontAwesomeIcon icon={faSearch} style={styles.linkIcon} />
-          </Link> | {" "}
-          <Link to="/collection" className="d-flex" style={styles.link}>
-            <FontAwesomeIcon icon={faRecordVinyl} style={styles.linkIcon} />
-          </Link>
-        </nav>
-        <Outlet />
+      <nav
+        style={styles.nav}
+        className="d-flex"
+      >
+        <Link to="/" className="d-flex" style={styles.link}>
+          <FontAwesomeIcon icon={faSearch} style={styles.linkIcon} />
+        </Link>
+        {' '}
+        |
+        {' '}
+        <Link to="/collection" className="d-flex" style={styles.link}>
+          <FontAwesomeIcon icon={faRecordVinyl} style={styles.linkIcon} />
+        </Link>
+      </nav>
+      <Outlet />
     </BrowserRouter>
   );
 }
--- src/pages/Collection/Collection.js
+++ src/pages/Collection/Collection.js
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from 'react';
 // import PropTypes from 'prop-types'
 
-const Collection = () => {
-  return (
-    <div className="p-2">
-      <h2>Your Collection</h2>
-    </div>
-  )
-}
+const Collection = () => (
+  <div className="p-2">
+    <h2>Your Collection</h2>
+  </div>
+);
 
 // Collection.propTypes = {
 

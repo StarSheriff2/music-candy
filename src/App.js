008b812226cec89f4cd327caf564cd7b8d314dcc
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-02-24 03:34:18 -0600

    Create Navigation buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+import React from 'react';
+import { BrowserRouter, Link, Routes, Route, Outlet } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faRecordVinyl } from '@fortawesome/free-solid-svg-icons';
 import SearchPage from './pages/SearchPage/SearchPage';
+import Collection from './pages/Collection/Collection';
+import { height, style } from 'dom-helpers';
 
 function App() {
-  return (
-    <div className="border border-danger">
-      <SearchPage />
+  const styles = {
+    nav: {
+      border: "solid blue 2px",
+      height: "7rem",
+    },
+    link: {
+      width: "100%",
+    },
+    linkIcon: {
+      fontSize: "4rem",
+      color: "orange",
+    },
+  };
 
-    </div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <div className="border border-danger">
+            <SearchPage />
+          </div>
+          }
+        />
+        <Route path="collection" element={<Collection />}/>
+      </Routes>
+        <nav
+          style={styles.nav}
+          className="d-flex"
+        >
+          <Link to="/" className="d-flex" style={styles.link}>
+            <FontAwesomeIcon icon={faSearch} style={styles.linkIcon} />
+          </Link> | {" "}
+          <Link to="/collection" className="d-flex" style={styles.link}>
+            <FontAwesomeIcon icon={faRecordVinyl} style={styles.linkIcon} />
+          </Link>
+        </nav>
+        <Outlet />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Collection/Collection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+const Collection = () => {
+  return (
+    <div className="p-2">
+      <h2>Your Collection</h2>
+    </div>
+  )
+}
+
+// Collection.propTypes = {
+
+// }
+
+export default Collection;
